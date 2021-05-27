262caa1b0c25d1c0c9b3bc861ce6ba98879dfdd8
sagar2015 <73906755+sagar2015@users.noreply.github.com>
2021-05-26 20:49:35 -0400

    Calculator.js & .html uploaded using single function

--- calculator.js
+++ calculator.js
@@ -1,41 +1,71 @@
-function addition() {
-    let c = parseInt(document.getElementById('first').value) + parseInt(document.getElementById('second').value);
+// function addition() {
+//     let c = parseInt(document.getElementById('first').value) + parseInt(document.getElementById('second').value);
 
-    document.getElementById('operator').innerText = '+';
+//     document.getElementById('operator').innerText = '+';
 
-    document.getElementById('result').value = c;
-
-
-}
-
-function subtraction() {
-    let c = parseInt(document.getElementById('first').value) - parseInt(document.getElementById('second').value);
-
-    document.getElementById('operator').innerText = '-';
-
-    document.getElementById('result').value = c;
-}
-
-function multiply() {
-    let c = parseInt(document.getElementById('first').value) * parseInt(document.getElementById('second').value);
-
-    document.getElementById('operator').innerText = '*';
-
-    document.getElementById('result').value = c;
-}
-
-function division() {
-    let c = parseInt(document.getElementById('first').value) / parseInt(document.getElementById('second').value);
+//     document.getElementById('result').value = c;
 
 
-    document.getElementById('operator').innerText = '/';
+// }
 
-    document.getElementById('result').value = c;
-}
+// function subtraction() {
+//     let c = parseInt(document.getElementById('first').value) - parseInt(document.getElementById('second').value);
+
+//     document.getElementById('operator').innerText = '-';
+
+//     document.getElementById('result').value = c;
+// }
+
+// function multiply() {
+//     let c = parseInt(document.getElementById('first').value) * parseInt(document.getElementById('second').value);
+
+//     document.getElementById('operator').innerText = '*';
+
+//     document.getElementById('result').value = c;
+// }
+
+// function division() {
+//     let c = parseInt(document.getElementById('first').value) / parseInt(document.getElementById('second').value);
+
+
+//     document.getElementById('operator').innerText = '/';
+
+//     document.getElementById('result').value = c;
+// }
 
 function erase() {
     document.getElementById('first').value = 0;
     document.getElementById('second').value = 0;
     document.getElementById('result').value = 0;
 
+}
+
+function calculator(arithmatic_operator) {
+    let a = parseInt(document.getElementById('first').value);
+    let b = parseInt(document.getElementById('second').value);
+
+    switch (arithmatic_operator) {
+        case '+':
+            document.getElementById('result').value = a + b;
+            document.getElementById('operator').innerText = '+'
+            break;
+
+        case '-':
+            document.getElementById('result').value = a - b;
+            document.getElementById('operator').innerText = '-'
+
+            break;
+
+        case '*':
+            document.getElementById('result').value = a * b;
+            document.getElementById('operator').innerText = '*'
+
+            break;
+
+        case '/':
+            document.getElementById('result').value = a / b;
+            document.getElementById('operator').innerText = '/'
+
+            break;
+    }
 }
